e6b7db2816e01a14fc291eb93ed2286dc37856c6
addibox <addilee@bu.edu>
2019-01-02 05:25:37 -0500

    improved performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = ( props ) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component{
+    componentWillUpdate () {
+        console.log('[OrderSummary] WillUpdate')
+    }
+    
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map( igKey => {
             return <li key={igKey}>
                         <span style={{textTransform: 'capitalize'}}>{igKey}</span>
-                        : {props.ingredients[igKey]}
+                        : {this.props.ingredients[igKey]}
                     </li>
         });
 
-    return (
-        <>
-            <h3>Your Order</h3>
-            <p>Your delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: </strong>{props.price.toFixed(2)}</p>
-            <p>Continue to Checkout?</p>
-            <Button clicked={props.purchaseCanceled} btnType="Danger">CANCEL</Button>
-            <Button clicked={props.purchaseContinue} btnType="Success">CONTINUE</Button>
-        </>
-    )};   
+        return (
+            <>
+                <h3>Your Order</h3>
+                <p>Your delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: </strong>{this.props.price.toFixed(2)}</p>
+                <p>Continue to Checkout?</p>
+                <Button clicked={this.props.purchaseCanceled} btnType="Danger">CANCEL</Button>
+                <Button clicked={this.props.purchaseContinue} btnType="Success">CONTINUE</Button>
+            </>
+        );
+    }
+}
+
+
+      
         
 
 
 
-export default orderSummary;
+export default OrderSummary;
